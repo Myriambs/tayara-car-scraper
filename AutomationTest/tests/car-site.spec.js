95519151c441adbd7b69f5d29c470ca32ae79391
Myriambs <bensalahmeriam19@gmail.com>
2025-04-28 17:42:59 +0100

    few modif

--- AutomationTest/tests/car-site.spec.js
+++ AutomationTest/tests/car-site.spec.js
@@ -11,47 +11,37 @@ test('La page de voitures se charge', async ({ page }) => {
 
   test('filtre les voitures par nom', async ({ page }) => {
     await page.goto('http://localhost:3000'); // Remplace par l'URL de ton app
-    
     // Saisir un terme de recherche
     await page.fill("input[placeholder='Rechercher par nom...']", 'Corolla');
     const result = await page.locator("div[class='car-list'] p").textContent();
     // Vérifier que la voiture filtrée est affichée
     await expect(result).toBe("Aucune voiture trouvée.");
-    
-
   });
   
   test('filtre les voitures par prix', async ({ page }) => {
     await page.goto('http://localhost:3000'); // Remplace par l'URL de ton app
-    
     // Appliquer les filtres de prix
     await page.fill("input[placeholder='Prix min']", '10000');
     await page.fill("input[placeholder='Prix max']", '20000');
-    
     const result = await page.locator("div[class='car-list'] p").textContent();
     // Vérifier que la voiture filtrée est affichée
-    await expect(result).toBe("Aucune voiture trouvée.");
+    await expect(result).toBe("19000 TND");
   });
 
   test.skip('ouvrir un autre onglet et vérifier son URL et son contenu', async ({ page }) => {
     // Accéder à la page principale
     await page.goto('http://localhost:3000'); // Remplace par l'URL de ton app
-    
     // Attendre l'ouverture d'un nouvel onglet après le clic
     const [newPage] = await Promise.all([
       page.waitForEvent('popup'),  // Attendre l'événement de popup (nouvel onglet)
       page.click("a[href='https://www.tayara.tn/item/680ea07ec5beda2b51d1daaa/Voitures/Kairouan/Kairouan/Voiture_vendre_/']"), // Cliquer sur le lien
     ]);
-  
     // Vérifier l'URL de ce nouvel onglet
     await expect(newPage).toHaveURL('https://www.tayara.tn/item/680ea07ec5beda2b51d1daaa/Voitures/Kairouan/Kairouan/Voiture_vendre_/');
-  
     // Vérifier que la page contient le texte spécifique (ici un exemple de contenu de la page)
     // Par exemple, vérifier si un titre ou un texte est présent dans la nouvelle page
     await expect(newPage.locator('.text-gray-700.font-bold.text-2xl.font-arabic')).toHaveText('Voiture à vendre');
-    
     ;  // Exemple de texte dans la page
-  
     // Optionnel : Vérification de l'URL de l'onglet après une certaine interaction
     // Exemple : Tu pourrais effectuer un autre clic ou interaction dans le nouvel onglet et vérifier l'URL à nouveau.
   });
